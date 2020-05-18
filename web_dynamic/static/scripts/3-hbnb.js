f87462e0191ff52e4940abb2c0dce6e7a55a3f14
juanmarcos <juanmarcoscabezas@gmail.com>
2020-05-18 18:14:23 -0500

    Retrieving data from API

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -47,13 +47,41 @@ function checkStatus () {
 }
 
 function placesSearch() {
+  console.log('Entr2a');
   $.ajax({
     type: 'POST',
     url: 'http://localhost:5001/api/v1/places_search/',
-    data: {},
-    dataType: 'text',
+    data: '{}',
+    contentType: 'application/json',
+    dataType: 'json',
     success: function (data) {
-      console.log(data);
+      setPlaces(data);
+    },
+    error: function(err) {
+      console.log(err);
     }
   });
 }
+
+function setPlaces(places) {
+  console.log(places);
+}
+
+
+{/* <article>
+	  <div class="title_box">
+	    <h2>{{ place.name }}</h2>
+	    <div class="price_by_night">${{ place.price_by_night }}</div>
+	  </div>
+	  <div class="information">
+	    <div class="max_guest">{{ place.max_guest }} Guest{% if place.max_guest != 1 %}s{% endif %}</div>
+            <div class="number_rooms">{{ place.number_rooms }} Bedroom{% if place.number_rooms != 1 %}s{% endif %}</div>
+            <div class="number_bathrooms">{{ place.number_bathrooms }} Bathroom{% if place.number_bathrooms != 1 %}s{% endif %}</div>
+	  </div>
+	  <div class="user">
+            <b>Owner:</b> {{ place.user.first_name }} {{ place.user.last_name }}
+          </div>
+          <div class="description">
+	    {{ place.description | safe }}
+          </div>
+	</article> */}

34a85403afc44b6db12cd287836d312e3367f0b5
juanmarcos <juanmarcoscabezas@gmail.com>
2020-05-18 18:51:21 -0500

    Changing template

--- web_dynamic/4-hbnb.py
+++ web_dynamic/4-hbnb.py
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/3-hbnb', strict_slashes=False)
+@app.route('/4-hbnb', strict_slashes=False)
 def hbnb():
     """ Filter section dynamic """
     states = storage.all(State).values()
@@ -35,7 +35,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('3-hbnb.html',
+    return render_template('4-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,

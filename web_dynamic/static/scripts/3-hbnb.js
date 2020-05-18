e81238d278cfe6c82cfa297781fe77c716e4b36b
juanmarcos <juanmarcoscabezas@gmail.com>
2020-05-18 18:47:48 -0500

    Removing logs

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -112,23 +112,4 @@ function setPlaces(places) {
     article.appendChild(description);
     placesTag.appendChild(article);
   });
-
 }
-
-  // <article>
-  //   <div class="title_box">
-  //     <h2>{{ place.name }}</h2>
-  //     <div class="price_by_night">${{ place.price_by_night }}</div>
-  //   </div>
-  //   <div class="information">
-  //     <div class="max_guest">{{ place.max_guest }} Guest{% if place.max_guest != 1 %}s{% endif %}</div>
-  //     <div class="number_rooms">{{ place.number_rooms }} Bedroom{% if place.number_rooms != 1 %}s{% endif %}</div>
-  //     <div class="number_bathrooms">{{ place.number_bathrooms }} Bathroom{% if place.number_bathrooms != 1 %}s{% endif %}</div>
-  //   </div>
-  //   <div class="user">
-  //           <b>Owner:</b> {{ place.user.first_name }} {{ place.user.last_name }}
-  //   </div>
-  //    <div class="description">
-  //     {{ place.description | safe }}
-  //    </div>
-  // </article>
